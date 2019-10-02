654ab16eb416ddf738348f8e0b7f015b54dc66e5
Evan Lindsay <evanlindsay@live.com>
2019-10-02 12:17:05 -0700

    Reorganize editor plugin files

---
+++ addons/enjin/editor/EnjinPlugin.gd
@@ -0,0 +1,43 @@
+tool
+extends EditorPlugin
+
+# Class references
+var EditorMenus = preload("res://addons/enjin/editor/extensions/EditorMenus.gd")
+# Toolbar ui template
+var toolbar_tscn = preload("res://addons/enjin/editor/toolbar.tscn")
+# Editor's primary toolbar (e.g. Scene Project Debug Editor Help)
+var editor_toolbar = null
+# Toolbar ui instance
+var toolbar = null
+
+func _enter_tree():
+	if editor_toolbar == null:
+		editor_toolbar = EditorMenus.get_primary_toolbar(self)
+	reload_toolbar()
+
+func _exit_tree():
+	remove_from_toolbar()
+
+# Reload the toolbar
+func reload_toolbar():
+	remove_from_toolbar()
+	add_to_toolbar()
+
+# Add the plugin toolbar to the editor toolbar
+func add_to_toolbar():
+	if editor_toolbar == null:
+		return
+	toolbar = toolbar_tscn.instance()
+	toolbar.connect("pressed", self, "start_plugin")
+	editor_toolbar.add_child(toolbar, true)
+
+# Remove the plugin toolbar from the editor toolbar
+func remove_from_toolbar():
+	if toolbar == null:
+		return
+	editor_toolbar.remove_child(toolbar)
+	toolbar.queue_free()
+	toolbar = null
+
+func start_plugin():
+	print("Plugin Started")
