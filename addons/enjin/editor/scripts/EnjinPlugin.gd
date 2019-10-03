b789148131eeabdc2148346076a72f4c9605f453
Evan Lindsay <evanlindsay@live.com>
2019-10-03 11:34:26 -0700

    Add some hacks to help deal with hot-reloading pitfalls

--- addons/enjin/editor/scripts/EnjinPlugin.gd
+++ addons/enjin/editor/scripts/EnjinPlugin.gd
@@ -1,7 +1,9 @@
 tool
 extends EditorPlugin
 
-# Class references
+const directory_name = "enjin"
+
+# Script references
 var EditorHelper = preload("res://addons/enjin/editor/scripts/helpers/EditorHelper.gd")
 # Toolbar ui template
 var toolbar_tscn = preload("res://addons/enjin/editor/views/toolbar_menu.tscn")
@@ -11,17 +13,28 @@ var editor_toolbar: Node = null
 var toolbar: Node = null
 
 func _enter_tree():
+    connect("resource_saved", self, "resource_saved_listener")
     if editor_toolbar == null:
         editor_toolbar = EditorHelper.get_primary_toolbar(self)
+    remove_orphans(editor_toolbar)
     reload_toolbar()
+    print_debug("Enjin Plugin Enabled!")
 
 func _exit_tree():
     remove_from_toolbar()
+    disconnect("resource_saved", self, "resource_saved_listener")
+    print_debug("Enjin Plugin Disabled!")
 
 # Reload the toolbar
 func reload_toolbar():
     remove_from_toolbar()
     add_to_toolbar()
+
+# Disable and re-enable the plugin (mostly serves as a hot-reload fix)
+func reload_plugin():
+    var editor_interface: EditorInterface = get_editor_interface()
+    editor_interface.set_plugin_enabled(directory_name, false)
+    editor_interface.set_plugin_enabled(directory_name, true)
 
 # Add the plugin toolbar to the editor toolbar
 func add_to_toolbar():
@@ -39,5 +52,16 @@ func remove_from_toolbar():
     toolbar.queue_free()
     toolbar = null
 
-func start_plugin():
-    print("Plugin Started")
+# Removes orphaned nodes created by the plugin
+func remove_orphans(node: Node):
+    for child in node.get_children():
+        if (child.text != "Enjin"):
+            continue
+        node.remove_child(child)
+        child.queue_free()
+
+# Reloads the plugin if any scripts are saved
+func resource_saved_listener(ref: Resource):
+    var path = ref.resource_path
+    if "res://addons/enjin/" in path and ".gd" in path:
+        reload_plugin()
--- addons/enjin/editor/scripts/ToolbarMenu.gd
+++ addons/enjin/editor/scripts/ToolbarMenu.gd
@@ -1,16 +1,43 @@
 tool # required for the script to execute in the editor
 extends MenuButton
 
+const id_pressed = "id_pressed"
+const on_item_pressed = "_on_item_pressed"
+const home_page = "https://enjin.io/"
+const docs_page = "https://kovan.cloud.enjin.io/docs/enjin"
+
+# Script references
+var EditorHelper = preload("res://addons/enjin/editor/scripts/helpers/EditorHelper.gd")
+# Plugin reference
 var plugin: EditorPlugin = null setget set_plugin
 
-func set_plugin(plugin_in):
-    plugin = plugin_in
-
 func _ready():
-    get_popup().connect("id_pressed", self, "_on_item_pressed")
+    var error_code: int = get_popup().connect(id_pressed, self, on_item_pressed)
+    log_error(error_code)
+
+func _exit_tree():
+    get_popup().disconnect(id_pressed, self, on_item_pressed)
 
 func _on_item_pressed(id):
-    var text = get_popup().get_item_text(id)
-    match text:
-        "Home":
-            OS.shell_open("https://enjin.io/")
+    var error_code: int = 0
+
+    match id:
+        10:
+            error_code = OS.shell_open(home_page)
+        20:
+            error_code = OS.shell_open(docs_page)
+        30:
+            plugin.reload_plugin()
+
+    log_button_error(text, error_code)
+
+func set_plugin(plugin_in: EditorPlugin):
+    plugin = plugin_in
+
+func log_error(code: int):
+    if code > 0:
+        push_error("Error detected - code: %s" % code)
+
+func log_button_error(button_text: String, code: int):
+    if code > 0:
+        push_error("Error detected - button: %s, code: %s." % button_text % code)
