7e408154951a8a6eb94efe51df428208dc468f34
Evan Lindsay <evanlindsay@live.com>
2019-10-14 17:35:47 -0700

    Set app id and auth token when authenticating app

--- addons/enjin/sdk/TrustedPlatformClient.gd
+++ addons/enjin/sdk/TrustedPlatformClient.gd
@@ -8,9 +8,17 @@ const APPLICATION_JSON = "application/json"
 # Endpoints
 const GRAPHQL = "/graphql"
 const OAUTH_TOKEN = "/oauth/token"
+# Headers
+const AUTHORIZATION = "Authorization"
+const X_APP_ID = "X-App-Id"
+# JSON Keys
+const TOKEN_TYPE = "token_type"
+const ACCESS_TOKEN = "access_token"
 
 var url: String
 var http: EnjinHttp
+var auth_app_id
+var auth_token
 
 func _init(base_url: String = KOVAN_BASE):
     url = base_url
@@ -27,6 +35,7 @@ func auth_user(email: String, password: String, callback: EnjinCallback):
     http.enqueue(call, cb)
 
 func auth_app(app_id: int, secret: String, callback: EnjinCallback):
+    auth_app_id = app_id
     var body = {
         "grant_type": "client_credentials",
         "client_id": str(app_id),
@@ -45,7 +54,19 @@ func auth_user_callback(res: EnjinResponse):
     pass
 
 func auth_app_callback(res: EnjinResponse):
-    pass
+    if !res.is_success():
+        clear_auth()
+        return
+    var result: JSONParseResult = JSON.parse(res.get_body())
+    if result.get_error() != OK:
+        clear_auth()
+        return
+    var data = result.get_result()
+    auth_token = "%s %s" % [data[TOKEN_TYPE], data[ACCESS_TOKEN]]
+
+func clear_auth():
+    auth_app_id = null
+    auth_token = null
 
 func post(endpoint: String, body) -> EnjinCall:
     var call = EnjinCall.new()
@@ -57,4 +78,8 @@ func post(endpoint: String, body) -> EnjinCall:
 func graphql_request(body):
     var call = post(GRAPHQL, to_json(body))
     call.set_content_type(APPLICATION_JSON)
+    if auth_app_id != null:
+        call.add_header(X_APP_ID, auth_app_id)
+    if auth_token != null:
+        call.add_header(AUTHORIZATION, auth_token)
     return call
