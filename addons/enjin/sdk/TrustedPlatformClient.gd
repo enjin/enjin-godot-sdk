40a6f22e8bfea058c43b8b017133faa7053c4684
TheoXD <theo.hallenius@gmail.com>
2019-11-25 03:26:24 +0100

    Replacing of EnjinGraphqlSchema with QueryBuilder

--- addons/enjin/sdk/TrustedPlatformClient.gd
+++ addons/enjin/sdk/TrustedPlatformClient.gd
@@ -7,6 +7,7 @@ const EnjinHttp = preload("res://addons/enjin/sdk/http/EnjinHttp.gd")
 const EnjinHosts = preload("res://addons/enjin/sdk/http/EnjinHosts.gd")
 const EnjinAuthService = preload("res://addons/enjin/sdk/services/EnjinAuthService.gd")
 const EnjinUserService = preload("res://addons/enjin/sdk/services/EnjinUserService.gd")
+const EnjinGraphqlQueryBuilder = preload("res://addons/enjin/sdk/graphql/EnjinGraphqlQueryBuilder.gd")
 
 var _base_url: String
 var _http: EnjinHttp
@@ -14,14 +15,20 @@ var _state: TrustedPlatformState setget ,get_state
 var _middleware: TrustedPlatformMiddleware
 var _auth_service: EnjinAuthService setget ,auth_service
 var _user_service: EnjinUserService setget ,user_service
+var _query_builder: EnjinGraphqlQueryBuilder.GQLBuilder setget ,query_builder
 
 func _init(base_url: String = EnjinHosts.KOVAN, port: int = 443, use_ssl: bool = true, verify_host: bool = true):
     _base_url = base_url
     _http = EnjinHttp.new(_base_url, port, use_ssl, verify_host)
     _state = TrustedPlatformState.new()
-    _middleware = TrustedPlatformMiddleware.new(_http, _state)
+    
+    _query_builder = EnjinGraphqlQueryBuilder.GQLBuilder.new()
+    _query_builder.add_folder("res://addons/enjin/sdk/graphql/templates/")
+    
+    _middleware = TrustedPlatformMiddleware.new(_http, _state, _query_builder)
     _auth_service = EnjinAuthService.new(_state, _middleware)
     _user_service = EnjinUserService.new(_middleware)
+    #TODO: scan default folder
 
 func get_state() -> TrustedPlatformState:
     return _state
@@ -31,3 +38,6 @@ func auth_service() -> EnjinAuthService:
 
 func user_service() -> EnjinUserService:
     return _user_service
+
+func query_builder() -> EnjinGraphqlQueryBuilder.GQLBuilder:
+    return _query_builder
--- addons/enjin/sdk/services/EnjinUserService.gd
+++ addons/enjin/sdk/services/EnjinUserService.gd
@@ -1,7 +1,7 @@
 extends Reference
 
 const TrustedPlatformMiddleware = preload("res://addons/enjin/sdk/TrustedPlatformMiddleware.gd")
-const EnjinGraphqlSchema = preload("res://addons/enjin/sdk/graphql/EnjinGraphqlSchema.gd")
+#const EnjinGraphqlSchema = preload("res://addons/enjin/sdk/graphql/EnjinGraphqlSchema.gd")
 
 var _middleware: TrustedPlatformMiddleware
 
@@ -9,16 +9,21 @@ func _init(middleware: TrustedPlatformMiddleware):
     _middleware = middleware
 
 func get_user(input: GetUserInput, options: Dictionary = {}):
-    _middleware.submit_gql_request(EnjinGraphqlSchema.get_user(input), options)
+    #_middleware.submit_gql_request(EnjinGraphqlSchema.get_user(input), options)
+    _middleware.execute_gql_query("GetUserQuery", input.create(), options)
 
 func get_users(input: GetUserInput, options: Dictionary = {}):
-    _middleware.submit_gql_request(EnjinGraphqlSchema.get_users(input), options)
+    _middleware.execute_gql_query("GetUsersQuery", input.create(), options)
+    #_middleware.submit_gql_request(EnjinGraphqlSchema.get_users(input), options)
 
 func create_user(input: CreateUserInput, options: Dictionary = {}):
-    _middleware.submit_gql_request(EnjinGraphqlSchema.create_user(input), options)
+    _middleware.execute_gql_query("CreateUserMutation", input.create(), options)
+    #_middleware.submit_gql_request(EnjinGraphqlSchema.create_user(input), options)
 
 func update_user(input: UpdateUserInput, options: Dictionary = {}):
-    _middleware.submit_gql_request(EnjinGraphqlSchema.update_user(input), options)
+    _middleware.execute_gql_query("UpdateUserMutation", input.create(), options)
+    #_middleware.submit_gql_request(EnjinGraphqlSchema.update_user(input), options)
 
 func delete_user(input: DeleteUserInput, options: Dictionary = {}):
-    _middleware.submit_gql_request(EnjinGraphqlSchema.delete_user(input), options)
+    _middleware.execute_gql_query("DeleteUserMutation", input.create(), options)
+    #_middleware.submit_gql_request(EnjinGraphqlSchema.delete_user(input), options)
