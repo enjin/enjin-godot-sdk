d214110be4db45d20b327a417298f0d6eec95e8d
Evan Lindsay <evanlindsay@live.com>
2019-12-22 20:30:27 -0800

    Add lifebar to hud

--- addons/enjin/example/scripts/Platformer.gd
+++ addons/enjin/example/scripts/Platformer.gd
@@ -1,4 +1,5 @@
 extends Node2D
+signal update_hud
 
 export var respawn_height = 1500
 
@@ -7,7 +8,14 @@ func _ready():
 
 func _process(delta):
     if $Player.position.y > respawn_height:
+        $Player.health = max(0, $Player.health - 1)
+
+        if $Player.health == 0:
+            get_tree().reload_current_scene()
+            return
+
         respawn($Player)
+        emit_signal("update_hud", $Player)
 
 func respawn(player):
     player.position.x = $Spawn.position.x
