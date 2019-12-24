286dd92e9e34c64687b776f47aaab6f04572238d
Evan Lindsay <evanlindsay@live.com>
2019-12-23 16:49:54 -0800

    Create test environment

--- addons/enjin/example/scripts/PlayerHud.gd
+++ addons/enjin/example/scripts/PlayerHud.gd
@@ -2,4 +2,4 @@ extends Container
 
 func update_hud(player):
     $CoinDisplay/CoinCount.text = str(player.coins)
-    $HealthDisplay/Scaler/Progress.value = player.health
+    $HealthBar/Progress.value = player.health
