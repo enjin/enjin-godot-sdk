4021558c4227af30418080329a9ad577b5dda067
Evan Lindsay <evanlindsay@live.com>
2020-02-26 14:46:56 -0800

    Inprove slime attack

--- addons/enjin/example/scripts/client/Slime.gd
+++ addons/enjin/example/scripts/client/Slime.gd
@@ -4,15 +4,19 @@ const FLOOR: Vector2 = Vector2(0, -1)
 
 export var speed = 100
 export var gravity = 30
-export var player_distance_threshold = 100
+export var player_detect_distance = 200
 export var attack_cooldown = 2
 
-onready var player = $"../../../Player"
 onready var ray = $LeftGroundRay
+onready var attack_area = $AttackLeft
 
+var player
 var velocity = Vector2(-speed, 0)
 var moving_left = true;
 var attack_cooldown_remaining = 0
+
+func _ready():
+    player = get_tree().get_nodes_in_group("player")[0]
 
 func _process(delta):
     var distance = get_global_position().distance_to(player.get_global_position())
@@ -22,9 +26,10 @@ func _process(delta):
 
     attack_cooldown_remaining = max(0, attack_cooldown_remaining - delta)
 
-    if distance < player_distance_threshold:
-        _set_moving_and_direction(x_diff > 0, false)
-        if attack_cooldown_remaining == 0:
+    if distance < player_detect_distance:
+        var attack = attack_area.overlaps_body(player)
+        _set_moving_and_direction(x_diff > 0, !attack)
+        if attack and attack_cooldown_remaining == 0:
             $AnimatedSprite.set_animation("attack")
             attack_cooldown_remaining = attack_cooldown
     elif !ray.is_colliding():
@@ -37,6 +42,7 @@ func _set_moving_and_direction(facing_left = true, moving = true):
         moving_left = facing_left
         $AnimatedSprite.flip_h = !moving_left
         ray = $LeftGroundRay if moving_left else $RightGroundRay
+        attack_area = $AttackLeft if moving_left else $AttackRight
 
     if moving:
         velocity = Vector2(-speed if facing_left else speed, velocity.y)
@@ -54,5 +60,7 @@ func _animation_complete():
 
 
 func _on_frame_change():
-    if $AnimatedSprite.animation == "attack" and $AnimatedSprite.frame == 2:
+    var is_attack_anim = $AnimatedSprite.animation == "attack"
+    var is_hit_frame = $AnimatedSprite.frame == 2
+    if is_attack_anim and is_hit_frame and attack_area.overlaps_body(player):
         player.damage(1)
