6cff72a92f6c9c0288a27206314e659b28a91b76
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-03-27 15:39:22 -0700

    - EDT-3988 Added pause-resume-quit functionality

---
+++ addons/enjin/example/scripts/client/PauseMenu.gd
@@ -0,0 +1,26 @@
+extends Popup
+
+func _process(delta):
+    var escape = Input.is_action_just_released("ui_cancel")
+    
+    if escape and visible:
+        _resume()
+    elif escape and not visible and not get_tree().paused:
+        _pause()
+
+func _on_resume():
+    _resume()
+
+func _on_options():
+    pass # Replace with function body.
+
+func _on_quit():
+    get_tree().quit()
+
+func _pause():
+    get_tree().paused = true
+    show()
+
+func _resume():
+    get_tree().paused = false
+    hide()
