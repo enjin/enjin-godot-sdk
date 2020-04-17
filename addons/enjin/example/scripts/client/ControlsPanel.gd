0e87ac925fb62eb42737af6fd02c7bf3b3fcf6f9
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-04-17 14:47:17 -0700

    EDT-26 Enabled hotkeys for controls menu

--- addons/enjin/example/scripts/client/ControlsPanel.gd
+++ addons/enjin/example/scripts/client/ControlsPanel.gd
@@ -1,6 +1,11 @@
 extends PopupPanel
 signal start
 
+func show():
+    .show()
+    $VBox/MarginContainer/Button.grab_focus()
+    $VBox/MarginContainer/Button.grab_click_focus()
+
 func _on_btn_mouse_entered():
     $HighlightSFX.play(0)
 
