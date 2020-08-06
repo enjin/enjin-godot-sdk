108edffdf41dcbab2608a7592a2fce611057da15
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-08-06 13:14:23 -0700

    - Removed EnjinCallback as a preload in BaseSchema.gd - Set SCHEMA in ProjectClient to "app" - Separated variable storage functionality from EnjinGraphqlRequest into EnjinVariableHolder

--- addons/enjin/sdk/graphql/EnjinGraphqlRequest.gd
+++ addons/enjin/sdk/graphql/EnjinGraphqlRequest.gd
@@ -1,20 +1,9 @@
-extends Reference
+extends "res://addons/enjin/sdk/graphql/EnjinVariableHolder.gd"
 
-var _vars: Dictionary = {} setget , get_vars
 var _namespace: String setget , get_namespace
 
-func _init(namespace: String):
+func _init(namespace: String).():
     _namespace = namespace
-
-func set(key: String, value):
-    _vars[key] = value
-    return self
-
-func is_set(key: String) -> bool:
-    return _vars.has(key)
-
-func get_vars() -> Dictionary:
-    return _vars
 
 func get_namespace() -> String:
     return _namespace
