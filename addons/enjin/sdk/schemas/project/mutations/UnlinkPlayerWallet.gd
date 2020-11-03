f0aff4aa53fd0c9c712dc9f463d213fd26bf9cb8
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-11-03 15:04:58 -0800

    - Removed UnlinkPlayerWallet mutation - Set variable key in UnlinkWallet to correct value

--- addons/enjin/sdk/schemas/project/mutations/UnlinkPlayerWallet.gd
+++
@@ -1,8 +0,0 @@
-extends "res://addons/enjin/sdk/graphql/EnjinGraphqlRequest.gd"
-class_name UnlinkPlayerWallet
-
-func _init().("enjin.sdk.project.UnlinkPlayerWallet"):
-    pass
-
-func eth_address(eth_address: String) -> UnlinkPlayerWallet:
-    return set_variable("address", eth_address)
